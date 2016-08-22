a88bba7925ee481706efe610ce5a0cd6ae044319
Justus Perlwitz <mail@justusperlwitz.com>
2016-08-23 00:06:03 +0200

    View: Helpful explainers

--- application/View.elm
+++ application/View.elm
@@ -17,14 +17,14 @@ view model =
             , small [] [ text " - SaaS Business Model Calculator" ]
             ]
         , div [ class "row" ]
-            [ div [ class "col-xs-6 col-md-3" ] [ controls model ]
+            [ div [ class "col-xs-6 col-md-3" ]  (controls model)
             , div [ class "col-xs-6 col-md-3 col-md-push-6" ] (results model)
             , div [ class "col-xs-12 col-md-6 col-md-pull-3" ] (numbers model)
             ]
         ]
 
 
-controls : Model -> Html Msg
+controls : Model -> List (Html Msg)
 controls model =
     let
         controlLabel labelText =
@@ -44,22 +44,22 @@ controls model =
                     []
                 ]
     in
-        Html.form [ class "form-horizontal" ]
+        [ Html.form [ class "form-horizontal" ]
             [ h2 [] [ text "Parameters" ]
             , div [ class "form-group" ]
-                [ controlLabel "Months:"
+                [ controlLabel "Months"
                 , numberInput model.months SetMonths 1 100 1
                 ]
             , div [ class "form-group" ]
-                [ controlLabel "Churn Rate (%): "
+                [ controlLabel "Churn Rate (%)"
                 , numberInput (model.churnRate * 100 |> round) SetChurnRate 1 100 1
                 ]
             , div [ class "form-group" ]
-                [ controlLabel "Customer Growth p.m.: "
+                [ controlLabel "Customer Growth per Month"
                 , numberInput model.customerGrowth SetCustomerGrowth 0 1000 10
                 ]
             , div [ class "form-group" ]
-                [ controlLabel "Revenue per Customer p.m.: "
+                [ controlLabel "Revenue per Customer per Month"
                 , numberInput model.initialRevenue SetInitialRevenue 0 1000 10
                 ]
             , div [ class "form-group" ]
@@ -67,10 +67,26 @@ controls model =
                 , numberInput model.cac SetCAC 0 5000 5
                 ]
             , div [ class "form-group" ]
-                [ controlLabel "Operation costs"
+                [ controlLabel "Fixed Operation costs"
                 , numberInput model.opCost SetOpCost 0 1000 100
                 ]
             ]
+        , h4 [] [ text "Explanation" ]
+        , dl []
+            [ dt [] [ text "Months" ]
+            , dd [] [ text "Length of business forecast" ]
+            , dt [] [ text "Churn Rate" ]
+            , dd [] [ text "Percentage of customers leaving monthly" ]
+            , dt [] [ text "Customer Growth per Month" ]
+            , dd [] [ text "Number of customers signing up in one month" ]
+            , dt [] [ text "Revenue per customer per Month" ]
+            , dd [] [ text "Revenue for one customer through subscription fees or similar" ]
+            , dt [] [ text "Customer Acquisition Cost" ]
+            , dd [] [ text "Cost related to acquiring one customer" ]
+            , dt [] [ text "Fixed Operation Cost" ]
+            , dd [] [ text "Fixed costs such as salaries, rent" ]
+            ]
+        ]
 
 
 numbers : Model -> List (Html Msg)
@@ -168,5 +184,20 @@ results model =
             , dd [] ltvcac
             , dt [] [ text "Minimum Cumulative Earnings" ]
             , dd [] minimumCumulativeEarnings
+            ]
+        , h4 [] [ text "Explanation" ]
+        , dl []
+            [ dt [] [ text "Earnings positive" ]
+            , dd [] [ text "Month at which earnings cross 0 " ]
+            , dt [] [ text "Cumulative Earnings positive" ]
+            , dd [] [ text "Month at which cumulative earnings (e.g. bank account) cross 0" ]
+            , dt [] [ text "Average Customer Lifetime" ]
+            , dd [] [ text "Average number of months a customer stays with this business " ]
+            , dt [] [ text "Customer Lifetime Value" ]
+            , dd [] [ text "Average revenue earned through one customer over the whole lifetime " ]
+            , dt [] [ text "LTV over CAC" ]
+            , dd [] [ text "Ratio of Lifetime Value over Customer Acquisition Cost. Less than 1 means acquisition will not yield in profit." ]
+            , dt [] [ text "Minimum Cumulative Earnings" ]
+            , dd [] [ text "Lowest point in cumulative earnings. Bank account needs to be able to shoulder this." ]
             ]
         ]
