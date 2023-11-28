66b699cc4baa78350d6dd907a6021522cfbb454f
Justus Perlwitz <hello@justus.pw>
2023-11-28 14:03:13 +0900

    Refactor Results

--- src/lib/components/Results.svelte
+++ src/lib/components/Results.svelte
@@ -1,15 +1,31 @@
+<script lang="ts">
+    const ebitPositiveMonth = 7;
+    const cumulativeEbitPositiveMonth = 10;
+    const averageCustomerLifeMonths = 33;
+    const customerLifetimeValue = 990;
+    const ltvOverCac = 22;
+    const minimimumCumulativeEbit = -345;
+    const currency = "$";
+</script>
+
 <h2>Results</h2>
 <dl>
     <dt>EBIT positive</dt>
-    <dd>At month <strong>7</strong></dd>
+    <dd>At month <strong>{ebitPositiveMonth}</strong></dd>
     <dt>Cumulative EBIT positive</dt>
-    <dd>At month <strong>10</strong></dd>
+    <dd>At month <strong>{cumulativeEbitPositiveMonth}</strong></dd>
     <dt>Average Customer Life</dt>
-    <dd><strong>33</strong> months</dd>
+    <dd><strong>{averageCustomerLifeMonths}</strong> months</dd>
     <dt>Customer Lifetime Value</dt>
-    <dd><strong class="text-success">990</strong> $</dd>
+    <dd>
+        <strong class="text-success">{customerLifetimeValue}</strong>
+        {currency}
+    </dd>
     <dt>LTV over CAC</dt>
-    <dd><span class="text-success">22</span></dd>
+    <dd><span class="text-success">{ltvOverCac}</span></dd>
     <dt>Minimum Cumulative EBIT</dt>
-    <dd><strong class="text-warning">-345</strong> $</dd>
+    <dd>
+        <strong class="text-warning">{minimimumCumulativeEbit}</strong>
+        {currency}
+    </dd>
 </dl>
