37f27405d13f46db7591b6ba0bad633d5f891fed
Justus Perlwitz <hello@justus.pw>
2023-11-29 14:09:45 +0900

    Refactor TextArea

--- src/lib/components/Options.svelte
+++ src/lib/components/Options.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
     import Currency from "./form/Currency.svelte";
+    import TextArea from "./form/TextArea.svelte";
     import TextInput from "./form/TextInput.svelte";
 </script>
 
 <h3>Options</h3>
 <Currency />
 <TextInput label="Scenario Name" />
-<div class="form-group">
-    <label>Comment</label><textarea
-        class="form-control"
-        placeholder="Scenario Comment"
-    ></textarea>
-</div>
+<TextArea label="Comment" placeholder="Scenario Comment" />
 <a href="#" class="btn btn-danger btn-xs btn-block">Delete Scenario</a>
---
+++ src/lib/components/form/TextArea.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let label: string;
+    export let value: string | undefined = undefined;
+    export let placeholder: string | undefined = undefined;
+    // TODO a11y
+</script>
+
+<div class="form-group">
+    <label>{label}</label><textarea
+        class="form-control"
+        bind:value
+        placeholder={placeholder ?? label}
+    ></textarea>
+</div>
