b6daa85ac35b11585ade746a846011b3353a5f58
Justus Perlwitz <hello@justus.pw>
2023-11-29 14:15:51 +0900

    Factor out currency in TableBody

--- src/lib/components/numbers/TableBody.svelte
+++ src/lib/components/numbers/TableBody.svelte
@@ -1,136 +1,163 @@
+<script lang="ts">
+    const currency = "$";
+</script>
+
 <tbody
     ><tr class="warning"
         ><td class="text-right">1</td><td class="text-right">10</td><td
-            class="text-right"><strong class="text-success">300</strong> $</td
-        ><td class="text-right"><strong class="text-success">150</strong> $</td
-        ><td class="text-right"><strong class="text-success">235</strong> $</td
-        ><td class="text-right"><strong class="text-warning">-85</strong> $</td
-        ><td class="text-right"><strong class="text-warning">-85</strong> $</td
+            class="text-right"
+            ><strong class="text-success">300</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">150</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">235</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-85</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-85</strong> {currency}</td
         ></tr
     ><tr class="warning"
         ><td class="text-right">3</td><td class="text-right">19</td><td
-            class="text-right"><strong class="text-success">570</strong> $</td
-        ><td class="text-right"><strong class="text-success">285</strong> $</td
-        ><td class="text-right"><strong class="text-success">370</strong> $</td
-        ><td class="text-right"><strong class="text-warning">-85</strong> $</td
+            class="text-right"
+            ><strong class="text-success">570</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-warning">-240</strong> $</td
+            ><strong class="text-success">285</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">370</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-85</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-240</strong> {currency}</td
         ></tr
     ><tr class="warning"
         ><td class="text-right">5</td><td class="text-right">35</td><td
-            class="text-right"><strong class="text-success">1050</strong> $</td
-        ><td class="text-right"><strong class="text-success">525</strong> $</td
-        ><td class="text-right"><strong class="text-success">550</strong> $</td
-        ><td class="text-right"><strong class="text-warning">-25</strong> $</td
+            class="text-right"
+            ><strong class="text-success">1050</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-warning">-335</strong> $</td
+            ><strong class="text-success">525</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">550</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-25</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-335</strong> {currency}</td
         ></tr
     ><tr class="warning"
         ><td class="text-right">7</td><td class="text-right">66</td><td
-            class="text-right"><strong class="text-success">1980</strong> $</td
-        ><td class="text-right"><strong class="text-success">990</strong> $</td
-        ><td class="text-right"><strong class="text-success">955</strong> $</td
-        ><td class="text-right"><strong class="text-success">35</strong> $</td
+            class="text-right"
+            ><strong class="text-success">1980</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-warning">-310</strong> $</td
+            ><strong class="text-success">990</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">955</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">35</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-310</strong> {currency}</td
         ></tr
     ><tr class="warning"
         ><td class="text-right">9</td><td class="text-right">124</td><td
-            class="text-right"><strong class="text-success">3720</strong> $</td
+            class="text-right"
+            ><strong class="text-success">3720</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">1860</strong> $</td
+            ><strong class="text-success">1860</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">1720</strong> $</td
-        ><td class="text-right"><strong class="text-success">140</strong> $</td
-        ><td class="text-right"><strong class="text-warning">-75</strong> $</td
+            ><strong class="text-success">1720</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">140</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-warning">-75</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">11</td><td class="text-right">233</td><td
-            class="text-right"><strong class="text-success">6990</strong> $</td
+            class="text-right"
+            ><strong class="text-success">6990</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">3495</strong> $</td
+            ><strong class="text-success">3495</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">3160</strong> $</td
-        ><td class="text-right"><strong class="text-success">335</strong> $</td
-        ><td class="text-right"><strong class="text-success">460</strong> $</td
+            ><strong class="text-success">3160</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">335</strong> {currency}</td
+        ><td class="text-right"
+            ><strong class="text-success">460</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">14</td><td class="text-right">599</td><td
             class="text-right"
-            ><strong class="text-success">17970</strong> $</td
+            ><strong class="text-success">17970</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">8985</strong> $</td
+            ><strong class="text-success">8985</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">7975</strong> $</td
+            ><strong class="text-success">7975</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">1010</strong> $</td
+            ><strong class="text-success">1010</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">2665</strong> $</td
+            ><strong class="text-success">2665</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">16</td><td class="text-right">1124</td><td
             class="text-right"
-            ><strong class="text-success">33720</strong> $</td
+            ><strong class="text-success">33720</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">16860</strong> $</td
+            ><strong class="text-success">16860</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">14860</strong> $</td
+            ><strong class="text-success">14860</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">2000</strong> $</td
+            ><strong class="text-success">2000</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">6080</strong> $</td
+            ><strong class="text-success">6080</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">18</td><td class="text-right">2110</td><td
             class="text-right"
-            ><strong class="text-success">63300</strong> $</td
+            ><strong class="text-success">63300</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">31650</strong> $</td
+            ><strong class="text-success">31650</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">27820</strong> $</td
+            ><strong class="text-success">27820</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">3830</strong> $</td
+            ><strong class="text-success">3830</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">12660</strong> $</td
+            ><strong class="text-success">12660</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">20</td><td class="text-right">3960</td><td
             class="text-right"
-            ><strong class="text-success">118800</strong> $</td
+            ><strong class="text-success">118800</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">59400</strong> $</td
+            ><strong class="text-success">59400</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">52120</strong> $</td
+            ><strong class="text-success">52120</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">7280</strong> $</td
+            ><strong class="text-success">7280</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">25210</strong> $</td
+            ><strong class="text-success">25210</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">22</td><td class="text-right">7432</td><td
             class="text-right"
-            ><strong class="text-success">222960</strong> $</td
+            ><strong class="text-success">222960</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">111480</strong> $</td
+            ><strong class="text-success">111480</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">97750</strong> $</td
+            ><strong class="text-success">97750</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">13730</strong> $</td
+            ><strong class="text-success">13730</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">48935</strong> $</td
+            ><strong class="text-success">48935</strong> {currency}</td
         ></tr
     ><tr class="success"
         ><td class="text-right">24</td><td class="text-right">13950</td><td
             class="text-right"
-            ><strong class="text-success">418500</strong> $</td
+            ><strong class="text-success">418500</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">209250</strong> $</td
+            ><strong class="text-success">209250</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">183385</strong> $</td
+            ><strong class="text-success">183385</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">25865</strong> $</td
+            ><strong class="text-success">25865</strong> {currency}</td
         ><td class="text-right"
-            ><strong class="text-success">93645</strong> $</td
+            ><strong class="text-success">93645</strong> {currency}</td
         ></tr
     ></tbody
 >
