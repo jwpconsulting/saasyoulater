4cd76fd01e4d2c94d373b684c067582d48da3992
Justus Perlwitz <hello@justus.pw>
2023-11-29 14:14:36 +0900

    Refactor table head/body

---
+++ src/lib/components/numbers/TableHead.svelte
@@ -0,0 +1,10 @@
+<thead
+    ><tr
+        ><th class="text-center">Month</th><th class="text-center"
+            >Customers</th
+        ><th class="text-center">Revenue</th><th class="text-center"
+            >Gross Margin</th
+        ><th class="text-center">Expenses</th><th class="text-center">EBIT</th
+        ><th class="text-center">Cumulative EBIT</th></tr
+    ></thead
+>
