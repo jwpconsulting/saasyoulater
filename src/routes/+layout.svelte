3e13bd8e271b419fe9d943d9585ed022f296e453
Justus Perlwitz <hello@justus.pw>
2024-08-17 09:51:07 +0900

    Enable tailwind, with preflight disabled

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
-    // As soon as the original features have been recreated, tailwind can be
-    // added back here:
-    // import "$lib/app.css";
+    import "$lib/app.css";
 </script>
 
 <slot />
