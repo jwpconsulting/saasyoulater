36465ef10d1c2905b3a15145e924e18ac444da96
Justus Perlwitz <hello@justus.pw>
2024-08-17 09:54:52 +0900

    Split up layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
-<script>
+<script lang="ts">
     import "$lib/app.css";
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import Help from "$lib/components/Help.svelte";
 </script>
 
+<svelte:head>
+    <title>SaaS You Later</title>
+</svelte:head>
+
+<Header />
 <slot />
+<hr />
+<Help />
+<hr />
+<Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
     import Controls from "$lib/components/Controls.svelte";
-    import Footer from "$lib/components/Footer.svelte";
-    import Header from "$lib/components/Header.svelte";
-    import Help from "$lib/components/Help.svelte";
     import Numbers from "$lib/components/Numbers.svelte";
     import Options from "$lib/components/Options.svelte";
     import Results from "$lib/components/Results.svelte";
 </script>
 
-<svelte:head>
-    <title>SaaS You Later</title>
-</svelte:head>
-
-<Header />
 <div class="row">
     <div class="col-xs-6 col-md-2">
         <Controls />
@@ -23,7 +15,3 @@
     </div>
     <div class="col-xs-12 col-md-7 col-md-pull-3"><Numbers /></div>
 </div>
-<hr />
-<Help />
-<hr />
-<Footer />
